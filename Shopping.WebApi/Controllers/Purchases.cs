1a9d39b0fd097aea5a3f9a567c20884d124e2c81
Ivan Shalimov <ivan.shalimov@outlook.com>
2022-02-05 17:14:49 +0200

    Reorganize

---
+++ Shopping.WebApi/Controllers/Purchases.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Purchases : ControllerBase
+    {
+        //public async Task<ActionResult> Index()
+        //{
+
+        //}
+    }
+}
